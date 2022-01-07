381a692a6d49c2acbf42f03d64e1a97405208d56
oneso <oneso@DESKTOP-B1HSHGG>
2022-01-08 01:59:21 +0900

    20220108 데톱에서 커밋함 log4j2설정 전까지함

--- ex00/src/test/java/com/crow/persistence/TimeMapperTests.java
+++ ex00/src/test/java/com/crow/persistence/TimeMapperTests.java
@@ -21,8 +21,14 @@ public class TimeMapperTests {
     
     @Test
     public void testGetTime() {
-        /* log.info(timeMapper.getClass().getName()); */
+        log.info(timeMapper.getClass().getName());
         log.info(timeMapper.getTime());
         
+    }
+    
+    @Test
+    public void testGetTime2() {
+        log.info("getTime2");
+        log.info(timeMapper.getTime2());
     }
 }
